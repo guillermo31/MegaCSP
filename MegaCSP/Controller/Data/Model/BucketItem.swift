3e8cf17500555ebe4c9794a393626190b6cfd77a
Guillermo <memo33101@gmail.com>
2019-01-08 08:57:28 -0700

    Added Storyboards

---
+++ MegaCSP/Controller/Data/Model/BucketItem.swift
@@ -0,0 +1,21 @@
+//
+//  BucketItem.swift
+//  GuillermoDataApp
+//
+//  Created by Ramos aguirre, Guillermo on 11/30/18.
+//  Copyright © 2018 ctec. All rights reserved.
+//
+
+import Foundation
+
+public class BucketItem
+{
+    let itemContents : String
+    let itemAuthor : String
+    
+    init(contents: String, author : String)
+    {
+        itemContents = contents
+        itemAuthor = author
+    }
+}
---
+++ MegaCSP/View/ArtCell.swift
@@ -0,0 +1,16 @@
+//
+//  ArtCell.swift
+//  GuillermoCreativity
+//
+//  Created by Ramos aguirre, Guillermo on 11/7/18.
+//  Copyright © 2018 CTEC. All rights reserved.
+//
+
+import UIKit
+
+public class ArtCell: UICollectionViewCell
+{
+    @IBOutlet weak var artImage: UIImageView!
+    
+    @IBOutlet weak var artLabel: UILabel!
+}
